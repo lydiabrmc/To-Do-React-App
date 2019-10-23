ba262f05ac1ff642fdcf8d5fc5fdfb20137d4257
Lydia Joynes <lydiajoynesgmail.com>
2019-10-23 17:00:25 +0100

    implemented responsive update

--- to-do-application/src/TaskList.js
+++ to-do-application/src/TaskList.js
@@ -5,21 +5,14 @@ class Item extends React.Component {
     render() {
       return (
         <div className="row">
-          <div className="col-2">
+          <div className="col-6 col-md-8">
             <p className={this.props.complete && "complete"}>{this.props.text}</p>
           </div>
-          <div className="col-1">
-            <p>
-              {this.props.quantity}
-            </p>
-          </div>
-          <div className="col-1">
+          <div className="col-6 col-md-4">
             <button className="btn btn-info" disabled={this.props.complete}>
               Add
             </button>
-          </div>
-          <div className="col-1">
-            <button className="btn btn-danger">Delete</button>
+            <button className="float-right btn btn-danger">Delete</button>
           </div>
         </div>
       );
