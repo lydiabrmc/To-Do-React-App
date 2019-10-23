b17c0b8341109a6983dc131c216d6b41f83b4507
Lydia Joynes <lydiajoynesgmail.com>
2019-10-23 17:14:06 +0100

    implemented checking header file

--- to-do-application/src/header.js
+++ to-do-application/src/header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-            <h1>To Do List</h1>
+                <h1>To Do List</h1>
             </div>
         )
     }
