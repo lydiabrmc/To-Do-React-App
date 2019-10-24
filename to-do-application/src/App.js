38cba9e5e9d32a621c545e5835708bd907c0662a
Lydia Joynes <lydiajoynesgmail.com>
2019-10-24 08:09:12 +0100

    implemented new Header folder

--- to-do-application/src/App.js
+++ to-do-application/src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from "react";
-import Header from "./Header"
+import Header from "./Header1"
 import "./App.css";
 import AddTask from "./AddTask"
 import TaskCount from "./TaskCount";
@@ -9,7 +9,7 @@ import SubHeader from "./SubHeader"
 import CompleteTask from "./CompleteTask"
 
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="container">
