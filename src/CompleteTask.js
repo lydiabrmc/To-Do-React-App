3feb413708d0cc0a8e1b6c69fda6644688aa19f4
Lydia Joynes <lydiajoynesgmail.com>
2019-11-06 16:00:25 +0000

    implemented complete task alignment

--- src/CompleteTask.js
+++ src/CompleteTask.js
@@ -2,15 +2,15 @@ import React from "react"
 import "./App.css"
 
 class CompleteTask extends React.Component {
-    render() {
-      return (
-        <div className="row">
-          <div className="col-6 col-md-8">
-            <p className={this.props.complete && "complete"}>{this.props.text}</p>
-          </div>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-12 col-sm-8">
+          <p className={this.props.complete && "complete"}>{this.props.text}</p>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
 export default CompleteTask;
