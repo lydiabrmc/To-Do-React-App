d0ca7d27310e732a53047a1cff45247a2d8540e0
Lydia Joynes <lydiajoynesgmail.com>
2019-11-21 15:47:32 +0000

    Updates

---
+++ precache-manifest.2a23fcd3203aa91116787d205d894c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e2feb4b837de2d3ea5ebb200d631b5",
+    "url": "/To-Do-React-App/index.html"
+  },
+  {
+    "revision": "71752548972b1d280dc7",
+    "url": "/To-Do-React-App/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "507460a3af00672bf233",
+    "url": "/To-Do-React-App/static/css/main.e38378b5.chunk.css"
+  },
+  {
+    "revision": "71752548972b1d280dc7",
+    "url": "/To-Do-React-App/static/js/2.1628256f.chunk.js"
+  },
+  {
+    "revision": "507460a3af00672bf233",
+    "url": "/To-Do-React-App/static/js/main.75cf45c5.chunk.js"
+  },
+  {
+    "revision": "30dd86d55962105fde4b",
+    "url": "/To-Do-React-App/static/js/runtime-main.dbcc6d81.js"
+  }
+]);
